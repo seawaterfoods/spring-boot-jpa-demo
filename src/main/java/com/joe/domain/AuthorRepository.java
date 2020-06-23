eed847c165e0c64e68f00aaf703e34011ef6df54
seawaterfood <99390124@ss25.mcu.edu.tw>
2020-06-23 14:35:04 +0800

    JPA的基本使用配置: 自定義JPQL語句。

--- src/main/java/com/joe/domain/AuthorRepository.java
+++ src/main/java/com/joe/domain/AuthorRepository.java
@@ -1,16 +1,38 @@
 package com.joe.domain;
 
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 public interface AuthorRepository extends JpaRepository<Author,Long> {
 
 //    可以自訂自己要的方法
-    List<Author> findByPhoneAndNickName(String phone,String nickName);
+//    List<Author> findByPhoneAndNickName(String phone,String nickName);
+//
+//    List<Author> findDistinctByNickNameIgnoreCaseOrPhoneOrderBySignDateDesc(String nickName,String phone);
+//
+//    List<Author> findByNickNameLike(String nickName);
 
-    List<Author> findDistinctByNickNameIgnoreCaseOrPhoneOrderBySignDateDesc(String nickName,String phone);
+    @Query("select a from Author a where a.phone like %:phone%")
+    List<Author> findByPhone(@Param("phone") String phone);
 
-    List<Author> findByNickNameLike(String nickName);
+    @Query("select a.nickName,length(a.nickName) from Author a where a.nickName like %?1%")
+    List<Object[]> findArry(String nickName);
 
+    @Query("select a from Author a where a.nickName like %?1%")
+    List<Author> findByNickName(String nickName, Sort sort);
+
+    @Query(value = "select * from author where nick_name like %?1%",nativeQuery = true)
+    List<Author> findBySql(String nickName);
+
+
+    @Transactional
+    @Modifying
+    @Query("update Author a set a.nickName=?1 where a.phone=?2")
+    int setNickName(String nickName ,String phone);
 }
--- src/test/java/com/joe/AuthorTests.java
+++ src/test/java/com/joe/AuthorTests.java
@@ -7,6 +7,7 @@ import com.joe.domain.AuthorRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Sort;
 
 import java.util.Date;
 import java.util.List;
@@ -19,18 +20,41 @@ public class AuthorTests {
 
 //    @Test
     public void saveAuthorTest(){
-        Author author=new Author();
-        author.setNickName("PizzaHot");
-        author.setPhone("0228825252");
-        author.setSignDate(new Date());
-        authorRepository.save(author);
+//        Author author1=new Author();
+//        Author author2=new Author();
+//        Author author3=new Author();
+//        Author author4=new Author();
+//
+//        author1.setNickName("tkkinc");
+//        author1.setPhone("0226599247");
+//        author1.setSignDate(new Date());
+//        authorRepository.save(author1);
+//
+//        author2.setNickName("SubWay");
+//        author2.setPhone("0226579298");
+//        author2.setSignDate(new Date());
+//        authorRepository.save(author2);
+//
+//        author3.setNickName("BurgerKing");
+//        author3.setPhone("0221753018");
+//        author3.setSignDate(new Date());
+//        authorRepository.save(author3);
+//
+//        author4.setNickName("WindyBurger");
+//        author4.setPhone("0222227777");
+//        author4.setSignDate(new Date());
+//        authorRepository.save(author4);
     }
 
     @Test
     public void findAuthorTest(){
-//        List<Author> authors = authorRepository.findByPhoneAndNickName("0228825252","PizzaHot");
-        List<Author> authors = authorRepository.findDistinctByNickNameIgnoreCaseOrPhoneOrderBySignDateDesc("PizzaHot","0228825252");
+//        List<Author> authors = authorRepository.findBySql("o");
+//        List<Author> authors = authorRepository.findByPhone("0226");
+//        List<Object[]> arry =authorRepository.findArry("piz");
 
-        System.out.println(JSON.toJSONString(authors,true));
+//        List<Author> authors = authorRepository.findByNickName("bur",
+//                Sort.by(Sort.Direction.DESC,"signDate"));
+        int i =authorRepository.setNickName("PizzaHot","0228825252");
+//        System.out.println(JSON.toJSONString(authors,true));
     }
 }
